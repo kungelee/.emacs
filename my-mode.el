280fa90c63c029fd93d05a7474c5c2baf123cb3b
kun.ge <kunge.lee@gmail.com>
2023-05-14 17:01:55 +0800

    Update my-mode.el
    wallpaper set in emacs 29

--- my-mode.el
+++ my-mode.el
@@ -48,7 +48,43 @@
 ;            (define-key map (kbd "<f6>") 'kunge/swap-two-window)
             map))
             
+;;  ---------------------------------set background image start---------------------------------
+;;  --------------------------------- 2023年5月14日17:01:11--------------------------------------
+;; set background alpha
+(set-frame-parameter nil 'alpha 0.8)
 
+;; disable tool bar and menu bar
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; set default path for backgound images
+(defconst background-image-path "d:/background-image")
+
+;; set last image
+(defvar last-image nil)
+
+;; change background image
+(defun change-background-image ()
+  (interactive)
+  (let* ((images (cddr (directory-files background-image-path t))) ;; 图片列表
+	 (images-length (length images))                           ;; 列表长度
+	 (image-position (mod (random) images-length))             ;; 随机位置
+	 (new-image (nth image-position images)))                  ;; 初始化图片
+    (catch 'break
+	  (while t
+	    (if (equal new-image last-image)                       ;; 判断是否是上次设置过的图片
+		(setq new-image (nth (mod (random) images-length) images))
+		(throw 'break new-image))))
+    (wallpaper-set new-image)
+    ;; 保存最后一次设置的图片
+    (setq last-image new-image)))
+  
+(change-background-image)
+
+;; binding shortcut key
+(global-set-key (kbd "C-c b") 'change-background-image)
+
+;;  ---------------------------------set background image end --------------------------------
 
 ;;(define-minor-mode my-mode
 ;;  "Get your foos in the right places."
